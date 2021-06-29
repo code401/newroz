b57c879712dfe7b2a9296419fb89ac1d426a38d4
Naziur Rahman <naziurrahman14@gmail.com>
2021-06-30 01:45:47 +0600

    response code added

--- offer_preorder.php
+++ offer_preorder.php
@@ -1,21 +1,26 @@
 <?php
+
+
+//pre order delivery
+
+//type 1 is for single order , type 2 is for multi order
+//single order
+//http://localhost/newroz/task4/offer_preorder.php?mobileno=013129829383&address=dhaka&name=chair&type=1
+//multiple order
+//http://localhost/newroz/task4/offer_preorder.php?mobileno=013129829383&address=dhaka&name=chair,sofa&type=2
+
+
 isset($_REQUEST['mobileno'])?$mobileno=$_REQUEST['mobileno']:'';
 isset($_REQUEST['address'])?$address=$_REQUEST['address']:'';
 isset($_REQUEST['name'])?$productname=$_REQUEST['name']:'';
-if(isset($_REQUEST['address'])){
-    
 
+if(isset($_REQUEST['address'])){    
 
 if(strpos(strtolower($address),'dhaka')>-1){
-
-    $charge=60;
-  
-
+    $charge=60; 
 }
 else{
-
     $charge=100;
-
 }
 
 }
@@ -38,26 +43,26 @@ if ($result->num_rows > 0) {
 
   }
 
+  
+  //generate unique order id
 $orderid=substr(md5(rand(1,10000)),0,6).$orderid;
 
-
-
 if(isset($_REQUEST['type'])){
-    //single
+    //single product order
 if($_REQUEST['type']==1){
 
-    $name=$_REQUEST['name'];
-    
-
-  
-
-  
+    $name=$_REQUEST['name'];  
 
     $sql = "INSERT INTO product_order (name,orderid, mobile_no, address , charge,preorder)
 VALUES ('$name', '$orderid', '$mobileno', '$address' ,'$charge',1)";
 
 if ($conn->query($sql) === TRUE) {
-    echo "pre record created successfully";
+
+  $response='single product pre-order added';
+  http_response_code(200);
+  echo json_encode($response);
+  
+
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
@@ -65,17 +70,15 @@ if ($conn->query($sql) === TRUE) {
 
 }
 
-//multi
+//multiple product order
 if($_REQUEST['type']==2){
 
     $namelist=$_REQUEST['name'];
     $namearray = explode (",", $namelist);
     
-    for($i=0;$i<count($namearray);$i++){
+    for($i=0;$i<count($namearray);$i++){         
 
-         
-
-
+      
         $orderid=$orderid.$i;
         $sql = "INSERT INTO product_order (name,orderid, mobile_no, address , charge,preorder)
         VALUES ('$namearray[$i]', '$orderid', '$mobileno', '$address' ,'$charge',1)";
@@ -83,8 +86,11 @@ if($_REQUEST['type']==2){
         if ($conn->query($sql) === TRUE) {
 
 
-            //invoice
-            echo "multi pre record created successfully";
+            
+          $response='multi product pre-order added';
+          http_response_code(200);
+          echo json_encode($response);
+          echo '<br/>';
 
           } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
